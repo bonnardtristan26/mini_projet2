8954f95352bb9ec14a71bb6f179cf159d066dbae
JulianChedotal <julianchedotal.pro@gmail.com>
2026-03-16 17:35:01 +0100

    création du squelette
    Squelette des fonctions principale du projet

---
+++ partieClient.js
@@ -0,0 +1,25 @@
+let socket;
+
+function initChat() {
+
+}
+
+function connectToServer() {
+
+}
+
+function sendMessage() {
+
+}
+
+function receiveMessage() {
+
+}
+
+function displayMessage() {
+
+}
+
+function disconnect() {
+
+}
