529ba9a66ec84c26fe0ae1d7cbbe34a45b188548
BonnardTristan <bonnard.tristan26@gmail.com>
2026-03-30 10:37:16 +0200

    changement id logo

--- Client/partieClient.js
+++ Client/partieClient.js
@@ -16,7 +16,7 @@ const serversIcons = document.getElementById("servers-icons");
 const channelsList = document.getElementById("channels-list");
 const sidebarHeader = document.getElementById("sidebar-header");
 const centerSidebar = document.getElementById("center-sidebar");
-const logo = document.getElementById('mon-logo');
+const logo = document.getElementById('logo_le_discorde');
 const musique = document.getElementById('musique-easter-egg');
 
 let monPseudo = "";
